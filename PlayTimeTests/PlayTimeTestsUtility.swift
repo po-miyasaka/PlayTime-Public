a740b89d601eb84d5b83322902c4a6b1d624fdf6
miyasaka_kazutoshi <miyasaka_kazutoshi@yahoo.co.jp>
2019-08-03 00:01:17 +0900

    remove unnecessary files

---
+++ PlayTimeTests/PlayTimeTestsUtility.swift
@@ -0,0 +1,9 @@
+//
+//  PlayTimeTestsUtility.swift
+//  PlayTimeTests
+//
+//  Created by kazutoshi miyasaka on 2019/08/02.
+//  Copyright © 2019 po-miyasaka. All rights reserved.
+//
+
+import Foundation
